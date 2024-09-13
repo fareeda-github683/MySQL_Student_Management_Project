5a7f70a4a08522b8d5a0018a7e1270cdb5b68a8b
fareeda-github683 <fareeda.github@gmail.com>
2024-09-13 11:42:47 +0530

    1.Create_tables.sql

--- SMS_Src/1.Create_tables.sql
+++ SMS_Src/1.Create_tables.sql
@@ -1,1 +1,51 @@
+/* STUDENT MANAGEMENT SYSTEM 
+Schema &Tables
+      BY Fareeda S.
+*/
+      
 
+/*====================================CREATE DATABASE==============================================*/
+CREATE DATABASE StudentManegementSystem;
+USE StudentManagementSystem;
+
+/*=========================================CREATE TABLES==================================================*/
+
+/*===============CREATE STUDENT TABLE=================*/
+CREATE TABLE Students(
+StudentID INT AUTO_INCREMENT PRIMARY KEY,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+DateOfBirth DATE,
+Email VARCHAR(100),
+Phone VARCHAR(15),
+Address VARCHAR(255)
+) AUTO_INCREMENT=1001;
+
+/*===============CREATE COURSE TABLE=================*/
+CREATE TABLE Courses(
+CourseID INT AUTO_INCREMENT PRIMARY KEY,
+CourseName VARCHAR(100),
+CourseCode varchar(10),
+CourseDescription TEXT,
+Credits INT
+)AUTO_INCREMENT=101;
+
+/*===============CREATE ENROLLMENT TABLE=================*/
+CREATE TABLE Enrollments(
+EnrollmentID INT AUTO_INCREMENT PRIMARY KEY,
+StudentID INT,
+CourseID INT,
+EnrollmentDate DATE,
+FOREIGN KEY(StudentID) REFERENCES Students(StudentID),
+FOREIGN KEY(CourseID) REFERENCES courses(CourseID)
+);
+
+/*===============CREATE GRADES TABLE=================*/
+CREATE TABLE Grades(
+GradeID INT AUTO_INCREMENT PRIMARY KEY,
+StudentID INT,
+CourseID INT,
+Grade CHAR(1),
+FOREIGN KEY(StudentID) REFERENCES Students(StudentID),
+FOREIGN KEY(CourseID) REFERENCES courses(CourseID)
+);
